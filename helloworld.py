19fa796e998bff9762f7827fc1e4e7fc6501e7cf
stadu <stadu@umich.edu>
2019-09-09 16:57:21 -0400

    first commit

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+import random
+print("Dustin Stabinski")
+num1 = random.randrange(0, 100)
+num2 = random.randrange(0, 100)
+print(num1)
+print(num2)
+sum = num1 + num2
+average = sum / 2
+print("Sum = " + str(sum))
+print("Average = " + str(average))
